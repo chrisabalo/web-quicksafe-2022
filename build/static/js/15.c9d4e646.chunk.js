76d507f5ceeca6df219f28cff69822593abaa30c
chrisabalo <69984119+chrisabalo@users.noreply.github.com>
2022-11-14 23:52:36 +0000

    Delete build directory

--- build/static/js/15.c9d4e646.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpQuicksafe-web"]=this["webpackJsonpQuicksafe-web"]||[]).push([[15],{1085:function(e,t,n){"use strict";n.r(t);n(1);var s=n(6);t.default=function(){return Object(s.jsx)("h1",{children:" Ajouter un centre de sant\xe9 "})}}}]);
-//# sourceMappingURL=15.c9d4e646.chunk.js.map
